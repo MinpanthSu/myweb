3c305bb3517dbb6f52793945c526c07d9c98c4c5
MinpanthSu <minpanth260544@gmail.com>
2025-08-06 13:26:03 +0700

    updates

--- content.js
+++ content.js
@@ -256,7 +256,7 @@ function answerQ3() {
 const finalDialogues = [
   { text: "สุดท้ายแล้วนะ เค้าอยากบอกว่า รักเบ้บนะคั้บบ ><", image: "image/minsmile.png" },
   { text: "ขอบคุณที่อยู่ดูถึงตอนนี้นะงับ", image: "image/minlaugh.png" },
-  { text: "ไว้เค้าไปหาบ่อยๆนะ บ๊ะบายน้าาาา", image: "image/minbye.png" }
+  { text: "ไว้เค้าไปหาบ่อยๆนะ รักนะคนเก่งของเค้า บ๊ะบายน้าาาา", image: "image/minbye.png" }
 ];
 
 let finalDialogueIndex = 0;
